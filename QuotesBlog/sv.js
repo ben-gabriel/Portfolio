0e68748c6662d72878c40f6fe7e3312c703e1935
Sergio <ben.gabriel.w@gmail.com>
2022-03-04 12:50:29 -0300

    add login/register GET routes

--- QuotesBlog/sv.js
+++ QuotesBlog/sv.js
@@ -1,26 +1,55 @@
 const express = require("express");
-const { render } = require("express/lib/response");
 const app = express();
 const port = 2404;
 
 // -------- Database
 const {database} = require('./database');
+
+
 // -------- Settings
 app.set('view engine', 'ejs');
 app.set('views', 'QuotesBlog/views');
 
 
+// -------- Routes
 app.get('/', (req, res)=>{
     res.render('index');
 });
 
+app.get('login', (req, res)=>{
+    res.render('login');
+});
+
+app.get('register', (req, res)=>{
+    res.render('register');
+});
 
 app.listen(port);
+
+
+// -------- Session
+const session = require('express-session');
+const MongoDBStore  = require("connect-mongodb-session")(session);
+
+const store = new MongoDBStore({
+    uri: 'mongodb://localhost:27017',
+    databaseName: 'QuotesBlog',
+    collection: 'Sessions'
+});
+
+
+app.use(session({ 
+    secret: 'SecretWordExample:b94d58g2264d89e54a56s31hg7hky6...', 
+    cookie: { maxAge: 600000 },
+    saveUninitialized: true,
+    resave: false,
+    store: store
+}));
 
 // TO DO
 // -------------
 // -Get request-
-// Check if {isLoggedIn}
+// Check if {isLoggedIn} cookie is true/exist
 //     True: Res with User Info
 //     False: Set {isLoggedIn} cookie to false.
 //            *Check if request requires authentication* 
