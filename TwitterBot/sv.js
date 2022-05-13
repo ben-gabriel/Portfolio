78e51e1ce82ec5c691eb2a5346289bf31896821d
Sergio <ben.gabriel.w@gmail.com>
2022-05-13 09:24:19 -0300

    add tweet loolup sample

--- TwitterBot/sv.js
+++ TwitterBot/sv.js
@@ -1,5 +1,31 @@
-require('dotenv').config()
+require('dotenv').config();
+const { TwitterApi } = require('twitter-api-v2');
 
-console.log('-------------------');
-console.log(process.env);
-console.log(process.env.USER_ID)
+const api_key = process.env.TWITTER_API_KEY;
+const secret_key = process.env.TWITTER_API_SECRET_KEY;
+const bearer_token = process.env.TWITTER_BEARER_TOKEN;
+
+/*----------------- */
+const https = require('https');
+
+const options = {
+    hostname: 'api.twitter.com',
+    // port:2404,
+    path: '/2/tweets/1524971033349935105',
+    method: 'GET',
+    headers: {'Authorization': 'Bearer '+ bearer_token}
+};
+
+const req = https.request(options, res => {
+    console.log(`statusCode: ${res.statusCode}`);
+
+    res.on('data', d => {
+        process.stdout.write(d);
+    });
+});
+
+req.on('error', error => {
+    console.error(error);
+});
+
+req.end();
