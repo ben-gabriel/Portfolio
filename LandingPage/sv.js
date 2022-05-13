17022cb05a971c8d10af20e9057446d09d9e158d
Sergio <ben.gabriel.w@gmail.com>
2022-05-13 09:23:46 -0300

    fix links

--- LandingPage/sv.js
+++ LandingPage/sv.js
@@ -3,9 +3,11 @@ const app = express();
 const port = 2404;
 
 app.get('/', (req,res)=>{
+    console.log('\n\n');
     res.sendFile('./index.html',{root:__dirname})
 });
 app.get('/assets/:filename',(req,res)=>{
+    console.log('GET/assets/:',req.params.filename);
     res.sendFile('./assets/'+req.params.filename,{root:__dirname})
 });
 
